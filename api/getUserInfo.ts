e6b9f27de8acd520cbc155edee7e8531a3afbdf9
liuyunlong <>
2025-06-27 20:49:02 +0800

    hello api

---
+++ api/getUserInfo.ts
@@ -0,0 +1,8 @@
+import type { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const { username = "World" } = req.query;
+  return res.json({
+    message: `Hello ${username}! Welcome!`,
+  });
+}
